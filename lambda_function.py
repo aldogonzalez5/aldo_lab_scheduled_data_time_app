a33963b8db35cad75b20f07d134199bd34e247fc
Gonzalez <gonzal1@UPSTREAMACCTS.xom.com>
2023-06-28 17:48:50 -0600

    TI-775371: Solution of lab3

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,93 @@
+from datetime import datetime
+import statistics
+
+from service.constants import DATASET_PROVIDER, DATASET_NAME
 from corva import Api, Cache, Logger, ScheduledDataTimeEvent, scheduled
 
 
 @scheduled
 def lambda_handler(event: ScheduledDataTimeEvent, api: Api, cache: Cache):
-    """Insert your logic here"""
-    Logger.info('Hello, World!')
+# 3. Here is where you can declare your variables from the argument event: ScheduledDataTimeEvent and start using Api, Cache and Logger functionalities. 
+
+    # The scheduled app can declare the following attributes from the ScheduledDataTimeEvent: company_id: The company identifier; asset_id: The asset identifier; start_time: The start time of interval; end_time: The end time of interval
+    asset_id = event.asset_id
+    company_id = event.company_id
+    start_time = event.start_time
+    end_time = event.end_time
+
+# 4. Utilize the attributes from the ScheduledDataTimeEvent to make an API request to corva#wits or any desired time type dataset.
+    # You have to fetch the realtime drilling data for the asset based on start and end time of the event.
+    # start_time and end_time are inclusive so the query is structured accordingly to avoid processing duplicate data
+    # We are only querying for rop field since that is the only field we need. It is nested under data. We are using the SDK convenience method api.get_dataset. See API Section for more information on convenience method. 
+    records = api.get_dataset(
+        provider="corva",
+        dataset= "wits",
+        query={
+            'asset_id': asset_id,
+            'timestamp': {
+                '$gte': start_time,
+                '$lte': end_time,
+            }
+        },
+        sort={'timestamp': 1},
+        limit=500,
+        fields="data.rop"
+    )
+
+    record_count = len(records)
+
+    # Utilize the Logger functionality. The default log level is Logger.info. To use a different log level, the log level must be specified in the manifest.json file in the "settings.environment": {"LOG_LEVEL": "DEBUG"}. See the Logger documentation for more information.
+    Logger.debug(f"{asset_id=} {company_id=}")
+    Logger.debug(f"{start_time=} {end_time=} {record_count=}")
+
+
+# 5. Implementing some calculations
+    # Computing mean rop value from the list of realtime wits records
+    rop = statistics.mean(record.get("data", {}).get("rop", 0) for record in records)
+
+    # Utililize the Cache functionality to get a set key value. The Cache functionality is built on Redis Cache. See the Cache documentation for more information.
+    # Getting last exported timestamp from Cache 
+    last_exported_timestamp = int(cache.get(key='last_exported_timestamp') or 0)
+
+    # Making sure we are not processing duplicate data
+    if end_time <= last_exported_timestamp:
+        Logger.debug(f"Already processed data until {last_exported_timestamp=}")
+        return None
+
+# 6. This is how to set up a body of a POST request to store the mean rop data and the start_time and end_time of the interval from the event.
+    
+    output = {
+        "timestamp": event.end_time,
+        "asset_id": asset_id,
+        "company_id": company_id,
+        "provider": DATASET_PROVIDER,
+        "collection": DATASET_NAME,
+        "data": {
+            "rop": rop,
+            "start_time": start_time,
+            "end_time": end_time
+        },
+        "version": 1
+    }
+
+    # Utilize the Logger functionality.
+    Logger.debug(f"{asset_id=} {company_id=}")
+    Logger.debug(f"{start_time=} {end_time=} {record_count=}")
+    Logger.debug(f"{output=}")
+
+# 7. Save the newly calculated data in a custom dataset
+    try:
+        # Utilize the Api functionality. The data=outputs needs to be an an array because Corva's data is saved as an array of objects. Objects being records. See the Api documentation for more information.
+        response = api.post(
+            f"api/v1/data/{DATASET_PROVIDER}/{DATASET_NAME}/", data=[output],
+        )
+        response.raise_for_status()
+
+    # Utililize the Cache functionality to set a key value. The Cache functionality is built on Redis Cache. See the Cache documentation for more information. This example is setting the last timestamp of the output to Cache
+        cache.set(key='last_exported_timestamp', value=records[-1].get("timestamp") or end_time)
+    except Exception as ex:
+        Logger.debug(f"{response.text}")
+        Logger.debug(f"error: {str(ex)}")
+        raise ex
+    
+    return output
--- test/app_test.py
+++ test/app_test.py
@@ -1,10 +1,22 @@
+import json
+import os
+
 from corva import ScheduledDataTimeEvent
 from lambda_function import lambda_handler
+from service.constants import DATASET_PROVIDER, DATASET_NAME
 
+TEST_STREAMS_PATH =  os.path.join(os.path.abspath(os.path.dirname(__file__)), 'streams')
 
-def test_app(app_runner):
-    event = ScheduledDataTimeEvent(
-        company_id=1, asset_id=1234, start_time=1578291000, end_time=1578291300
-    )
-
-    app_runner(lambda_handler, event=event)
+class TestScheduledTimeApp:
+    def test_app(self, app_runner, requests_mock):
+        event = ScheduledDataTimeEvent(
+            company_id=1, asset_id=1234, start_time=1578291000, end_time=1578291300
+        )
+        stream_file = os.path.join(TEST_STREAMS_PATH, "corva_wits.json")
+        with open(stream_file, encoding="utf8") as raw_stream:
+            corva_wits = json.load(raw_stream)
+        requests_mock.get(f'https://data.localhost.ai/api/v1/data/corva/wits/', json=corva_wits)
+        requests_mock.post(f'https://data.localhost.ai/api/v1/data/{DATASET_PROVIDER}/{DATASET_NAME}/')
+        output = app_runner(lambda_handler, event=event)
+        assert output
+        assert output["data"]["rop"] == 3
